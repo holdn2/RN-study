0798ff5d35fa231c53c912e2919846ae64249ae1
holdn2 <yoocy01@gmail.com>
2024-08-19 15:10:13 +0900

    first commit

---
+++ src/component/Divider.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { View } from "react-native";
+
+export const Divider = () => {
+  return (
+    <View
+      style={{
+        alignSelf: "stretch",
+        borderWidth: 0.5,
+        marginHorizontal: 24,
+        borderColor: "gray",
+      }}
+    />
+  );
+};
+
+// export class Divider extends React.Component {
+//   render() {
+//     return (
+//       <View
+//         style={{
+//           alignSelf: "stretch",
+//           borderWidth: 0.5,
+//           marginHorizontal: 24,
+//           borderColor: "gray",
+//         }}
+//       />
+//     );
+//   }
+// }
---
+++ src/component/Icons.js
@@ -0,0 +1,18 @@
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+
+export const Icon = (props) => {
+  return <Ionicons name={props.name} size={props.size} color={props.color} />;
+};
+
+// export class Icon extends React.Component {
+//   render() {
+//     return (
+// <Ionicons
+//   name={this.props.name}
+//   size={this.props.size}
+//   color={this.props.color}
+// />
+//     );
+//   }
+// }
